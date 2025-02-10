874c39a36bcd7da2ae730cae6d9a65fefccecbb7
Kan-Ru Chen <kanru@kanru.info>
2025-02-10 23:15:32 +0900

    build: use reqwest with rustls

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,7 @@ use self::supervisor::Supervisor;
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
+    let _ = tokio_rustls::rustls::crypto::aws_lc_rs::default_provider().install_default();
 
     let flags = Pinka::from_env_or_exit();
 
