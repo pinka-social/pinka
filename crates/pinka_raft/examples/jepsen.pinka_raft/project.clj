7961f9f8ff1fafd9401c4b3198db7c09ff6e2741
Kan-Ru Chen <kanru@kanru.info>
2025-03-09 10:57:45 +0900

    tests(raft): upgrade jepsen to 0.3.8

--- crates/pinka_raft/examples/jepsen.pinka_raft/project.clj
+++ crates/pinka_raft/examples/jepsen.pinka_raft/project.clj
@@ -2,7 +2,7 @@
   :description "A Jepsen test for Pinka-Raft"
   :main jepsen.pinka-raft
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [jepsen "0.3.7"]
+                 [jepsen "0.3.8"]
                  [cheshire "5.13.0"]
                  [clj-http "3.13.0"]]
   :repl-options {:init-ns jepsen.pinka-raft})
