56ba8c3c6295d414dd44c9621566b34b6d8a54db
Remnant <185878324+Remnant-et-al@users.noreply.github.com>
2026-04-07 03:37:21 +0000

    Correct the content type for the javascript asset
    Firefox was warning me that the Content-Type for comments.js was text/plain and not
    text/javascript. This fixes that.

--- src/http/assets.rs
+++ src/http/assets.rs
@@ -1,3 +1,10 @@
-pub(super) async fn get_comments_js() -> &'static str {
-    include_str!("comments.js")
+use axum::{
+    response::IntoResponse,
+    http::{HeaderMap, header},
+};
+
+pub(super) async fn get_comments_js() -> impl IntoResponse {
+    let mut headers = HeaderMap::new();
+    headers.insert(header::CONTENT_TYPE, "text/javascript".parse().unwrap());
+    (headers, include_str!("comments.js"))
 }
